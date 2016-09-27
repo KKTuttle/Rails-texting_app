3ca238b15d8f526b4075c99b3fd803c072906428
cory olson and katie tuttle <cory+katie@>
2016-09-27 11:35:42 -0700

    refactor code for strava and facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,30 +2,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def strava
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Strava") if is_navigational_format?
-    else
-      session["devise.strava_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    self.custom_auth "strava"
   end
 
   def facebook
+    self.custom_auth "facebook"
+  end
+
+  def custom_auth(provider)
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => provider.capitalize) if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.#{provider.downcase}_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
-  end
-
-  def custom_auth do
-
   end
 
   def failure
